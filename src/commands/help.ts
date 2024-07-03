a91986fc0add611df29df04661efe0872a71daea
cginevra <cginevra@onestock-retail.com>
2024-07-03 18:27:43 +0200

    [#5] Add skill.sh command with colors & Description

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -4,6 +4,7 @@ function execute(_params: string[]): string {
   return '\\033[redm######################## \\033[yellowmHELP\\033[m ########################\\033[m\n\
 \\033[redm+\\033[m \\033[#03978Ameducation.sh\\033[m     \\033[redm:\\033[m Display my formations\n\
 \\033[redm+\\033[m \\033[#03978Amwork.sh\\033[m     \\033[redm:\\033[m Display my work experiences\n\
+\\033[redm+\\033[m \\033[#03978Amskill.sh\\033[m     \\033[redm:\\033[m List all my skills\n\
 \\033[redm+\\033[m \\033[#03978Amlink.sh\\033[m     \\033[redm:\\033[m List all my links\n\
 \\033[redm######################################################\\033[m';
 }
