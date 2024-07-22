38b5120b4591084562548f0aceb0c3ec4bb3f7ec
cginevra <cginevra@onestock-retail.com>
2024-07-22 17:58:23 +0200

    [#28] Enforce eslint and ts compile rules and use the same settings for all workspaces

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,15 +13,25 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   rules: {
-    "no-unused-vars": "off",
-    "for-direction": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
+    'no-unused-vars': 'off',
+    'for-direction': 'off',
+    '@typescript-eslint/array-type': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/ban-types': 'error',
+    '@typescript-eslint/class-literal-property-style': 'error',
+    '@typescript-eslint/consistent-generic-constructors': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/no-duplicate-enum-values': 'error',
+    '@typescript-eslint/no-duplicate-type-constituents': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
       {
-        "argsIgnorePattern": "^_",
-        "varsIgnorePattern": "^_",
-        "caughtErrorsIgnorePattern": "^_"
-      }
-    ]
-  }
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
 };
